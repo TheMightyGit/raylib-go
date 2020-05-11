90d83c219aed805acf3ab3db604ae3acfe680af8
Johnny Marshall <the.mighty.git@gmail.com>
2020-05-11 11:37:45 +0100

    try adding missing funcs

--- raylib/raylib.go
+++ raylib/raylib.go
@@ -808,16 +808,15 @@ type MaterialMap struct {
 
 type Model struct {
 	// Local transform matrix
-	Transform         Matrix
-	MeshCount         int32
-	Meshes            []Mesh
-	MaterialCount     int32
-	Materials         []Material
-	MeshMaterialCount int32
-	MeshMaterial      []int32
-	BoneCount         int32
-	Bones             []BoneInfo
-	BindPose          []Transform
+	Transform     Matrix
+	MeshCount     int32
+	Meshes        []Mesh
+	MaterialCount int32
+	Materials     []Material
+	MeshMaterial  []int32
+	BoneCount     int32
+	Bones         []BoneInfo
+	BindPose      []Transform
 }
 
 // newModelFromPointer - Returns new Model from pointer
