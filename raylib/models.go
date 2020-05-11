1b1280df7b7f00c5b221820aca87cdb178d87f19
Johnny Marshall <the.mighty.git@gmail.com>
2020-05-11 10:30:49 +0100

    try adding missing funcs

--- raylib/models.go
+++ raylib/models.go
@@ -6,6 +6,7 @@ package rl
 */
 import "C"
 import (
+	"log"
 	"unsafe"
 )
 
@@ -344,11 +345,11 @@ func SetMaterialTexture(material *Material, mapType int32, texture Texture2D) {
 func SetModelMeshMaterial(model Model, meshId int32, materialId int32) {
 	//log.Println(model)
 	cmodel := model.cptr()
-	//log.Println(cmodel)
+	log.Println(cmodel)
 	//time.Sleep(time.Second * 5)
 	cmeshId := (C.int)(meshId)
 	cmaterialId := (C.int)(materialId)
-	C.SetModelMeshMaterial(cmodel, cmeshId, cmaterialId)
+	C.SetModelMeshMaterial(&cmodel, cmeshId, cmaterialId)
 }
 
 // DrawModel - Draw a model (with texture if set)
