9d5d9ecd7c1df29e5456bddfa3d9999cfc72c3bf
Johnny Marshall <the.mighty.git@gmail.com>
2020-05-11 10:31:11 +0100

    try adding missing funcs

--- raylib/models.go
+++ raylib/models.go
@@ -349,7 +349,7 @@ func SetModelMeshMaterial(model Model, meshId int32, materialId int32) {
 	//time.Sleep(time.Second * 5)
 	cmeshId := (C.int)(meshId)
 	cmaterialId := (C.int)(materialId)
-	C.SetModelMeshMaterial(&cmodel, cmeshId, cmaterialId)
+	C.SetModelMeshMaterial(cmodel, cmeshId, cmaterialId)
 }
 
 // DrawModel - Draw a model (with texture if set)
