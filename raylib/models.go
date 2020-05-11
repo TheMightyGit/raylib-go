fcf91116c67e948d951b81aa1739a1c8b9a3f97c
Johnny Marshall <the.mighty.git@gmail.com>
2020-05-11 10:35:27 +0100

    try adding missing funcs

--- raylib/models.go
+++ raylib/models.go
@@ -342,7 +342,7 @@ func SetMaterialTexture(material *Material, mapType int32, texture Texture2D) {
 	C.SetMaterialTexture(cmaterial, cmapType, *ctexture)
 }
 
-func SetModelMeshMaterial(model Model, meshId int32, materialId int32) {
+func SetModelMeshMaterial(model *Model, meshId int32, materialId int32) {
 	//log.Println(model)
 	cmodel := model.cptr()
 	log.Println(cmodel)
